55de2d8f5aab50bce46df4fc597b41ae38324d9d
yoru9zine <yoru9zine@gmail.com>
2016-03-21 07:42:51 +0900

    Add source and README

---
+++ README.md
@@ -0,0 +1,18 @@
+# dusort
+
+Sort du output for impatient person.
+
+## install
+
+```
+go get github.com/yoru9zine/dusort
+go install github.com/yoru9zine/dusort
+```
+
+## Usage
+
+```
+du -sh /path/to/directory/* | dusort
+```
+
+- Press `ESC`, `Q` to exit
---
+++ dusort.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	var m sync.Mutex
+	if err := termbox.Init(); err != nil {
+		log.Fatalf("failed to initialize terminal: %s", err)
+	}
+	defer termbox.Close()
+
+	entryc := make(chan entry)
+	var first *entry
+	go readEntry(entryc)
+	go func() {
+		for e := range entryc {
+			m.Lock()
+			ee := e
+			if first == nil {
+				first = &ee
+			} else {
+				first = first.Insert(&ee)
+			}
+			draw(first.List())
+			m.Unlock()
+		}
+	}()
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return
+			}
+			switch ev.Ch {
+			case 'q':
+				return
+			}
+		}
+	}
+}
+
+func draw(entries []*entry) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	xmax, ymax := termbox.Size()
+	x, y := 0, 0
+	for _, e := range entries {
+		x = 0
+		for _, r := range e.String() {
+			termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
+			x++
+			if x > xmax {
+				break
+			}
+		}
+		y++
+		if y > ymax {
+			break
+		}
+	}
+	termbox.Flush()
+}
+
+type entry struct {
+	Name       string
+	SizeString string
+	Size       float64
+
+	Next *entry
+	Prev *entry
+}
+
+func (e *entry) String() string {
+	return fmt.Sprintf("%s\t%s", e.SizeString, e.Name)
+}
+
+func (e *entry) List() (list []*entry) {
+	ee := e
+	for {
+		if ee == nil {
+			break
+		}
+		list = append(list, ee)
+		ee = ee.Next
+	}
+	return
+}
+
+func (e *entry) Insert(n *entry) *entry {
+	ee := e
+	for {
+		if ee.Size <= n.Size {
+			if ee.Prev != nil {
+				ee.Prev.Next = n
+			}
+			n.Prev = ee.Prev
+			n.Next = ee
+			ee.Prev = n
+			return ee.First()
+		}
+		if ee.Next == nil {
+			ee.Next = n
+			n.Prev = ee
+			return ee.First()
+		}
+		ee = ee.Next
+	}
+}
+
+func (e *entry) First() *entry {
+	ee := e
+	for {
+		if ee.Prev == nil {
+			return ee
+		}
+		ee = ee.Prev
+	}
+}
+
+var sizeSuffix = map[byte]float64{}
+
+func init() {
+	n := 1.0
+	for _, r := range []byte{'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'} {
+		n = n * 1024
+		sizeSuffix[r] = n
+	}
+}
+
+func parseSize(s string) (float64, error) {
+	suffix := s[len(s)-1]
+	m, ok := sizeSuffix[suffix]
+	var num float64
+	var err error
+	if ok {
+		num, err = strconv.ParseFloat(s[:len(s)-1], 64)
+		num *= m
+	} else {
+		num, err = strconv.ParseFloat(s, 64)
+	}
+	return num, err
+}
+
+func readEntry(c chan entry) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		tokens := strings.Split(scanner.Text(), "\t")
+		sizeStr := tokens[0]
+		name := tokens[1]
+		size, err := parseSize(sizeStr)
+		if err != nil {
+			log.Printf("parse error %s: %s\n", sizeStr, err)
+		}
+		c <- entry{
+			Name:       name,
+			SizeString: sizeStr,
+			Size:       size,
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("error at readEntry: %s\n", err)
+	}
+	close(c)
+}
